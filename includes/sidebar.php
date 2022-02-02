64a65898cbe824389322fa99e168d6443a4cb6a6
Ramonas-Nojus <ramonas.nojus@gmail.com>
2022-02-02 17:17:00 +0200

    added user search div

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -47,6 +47,21 @@
                     </form><!--search form-->
                     <!-- /.input-group -->
                 </div>
+
+                <div class="well">
+                    <h4>Users Search</h4>
+                    <form action="search" method="post">
+                    <div class="input-group">
+                        <input name="search" type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button name="submit" class="btn btn-default" type="submit">
+                                <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                        </span>
+                    </div>
+                    </form><!--search form-->
+                    <!-- /.input-group -->
+                </div>
                 
                 
                 
