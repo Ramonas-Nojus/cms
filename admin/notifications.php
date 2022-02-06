9fc9b70d245cb722e6e8b209c56c6b8544335b82
Ramonas-Nojus <ramonas.nojus@gmail.com>
2022-02-06 19:14:08 +0200

    add request blocks in notifications

--- admin/notifications.php
+++ admin/notifications.php
@@ -9,7 +9,38 @@
         <?php include "includes/admin_navigation.php" ?>
         
         
-    
+    <style>
+
+    .frind_request {
+
+        font-size: 20px;
+        border: 3px solid;
+        border-radius: 10px;
+        width: max-content;
+        padding: 10px;
+        margin: 10px;
+
+    }
+
+    .b {
+        width:35%;
+        display:inline-table;
+        margin-bottom: 10px;
+        }
+
+
+    img {
+        border-radius: 50%;
+        width: 75px;
+        height: 75px;
+        object-fit: cover;
+        margin-right: 10px;
+
+    }
+
+    </style>
+
+
 
 <div id="page-wrapper">
 
@@ -26,7 +57,39 @@
             
             
 
+           <?php 
            
+           $user_id = $_SESSION['user_id'];
+           $username = $_SESSION['username'];
+
+           $get_request_query = query("SELECT * FROM requests WHERE to_id = '$user_id' AND to_username = '$username' ");
+           
+           while($row = mysqli_fetch_array($get_request_query)){
+               $to_username = $row['to_username'];
+               $from_username = $row['from_username'];
+
+               ?> 
+               
+               <div class="frind_request">
+               <?php 
+                   
+                   $users_image_query = query("SELECT * FROM users WHERE username = '$from_username' "); 
+
+                   while($row = mysqli_fetch_array($users_image_query)){
+                    $user_image = $row['user_image'];
+                    }
+                   
+                   ?>
+                   <div class="pull-left">
+                   <img src="/cms/images/<?php echo $user_image; ?>">
+                   </div>
+                   <a href="/cms/user_profile/<?php echo $from_username; ?>"><?php  echo $from_username; ?></a> wants to be your friend</br>
+                   <button class="b btn btn-primary">accept</button> <button class="b btn btn-danger">delete</button>
+               </div>
+               
+               <?php    //    echo $from_username." wants to be your friend";
+             }
+         ?>
             
     
             
