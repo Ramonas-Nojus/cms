46b5d1c69da10dd74e5a05d76ef393a760505734
Ramonas-Nojus <ramonas.nojus@gmail.com>
2022-02-28 20:18:53 +0200

    update returning posts by categorie with OOP

--- category.php
+++ category.php
@@ -1,5 +1,7 @@
 <?php  include "includes/db.php"; ?>
  <?php  include "includes/header.php"; ?>
+ <?php include "includes/class.autoload.php"; ?>
+
 
 
     <!-- Navigation -->
@@ -26,6 +28,8 @@
 
       echo "<h1>All posts with <<<b>$category</b>>>> categorie</h1>";
 
+      $postsByCat = new GetPostsByCat($post_category_id);
+
 
 if(isset($_SESSION['username']) && is_admin($_SESSION['username'])){
 
@@ -33,68 +37,19 @@ if(isset($_SESSION['username']) && is_admin($_SESSION['username'])){
 
 
 
-      $stmt1 = mysqli_prepare($connection, "SELECT post_id, post_title, post_user, post_date, post_image, post_content FROM posts WHERE post_category_id = ?");
-
-
-
-
-    } else {
-
-         $stmt2 = mysqli_prepare($connection, "SELECT post_id, post_title, post_user, post_date, post_image, post_content FROM posts WHERE post_category_id = ? AND post_status = ? ");
-
-         $published = 'published';
-
-    }
-
-
-    if(isset($stmt1)){
-
-        mysqli_stmt_bind_param($stmt1, "i", $post_category_id);
-
-        mysqli_stmt_execute($stmt1);
-
-        mysqli_stmt_bind_result($stmt1, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content);
-
-      $stmt = $stmt1;
-
-
-    }else {
-
-
-        mysqli_stmt_bind_param($stmt2, "is", $post_category_id, $published);
-
-        mysqli_stmt_execute($stmt2);
-
-        mysqli_stmt_bind_result($stmt2, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content);
-
-     $stmt = $stmt2;
-
-    }
-
-
-
-    // if(mysqli_stmt_num_rows($stmt) < 1) {
-
-
-
-         // echo "<h1 class='text-center'>No Post available for this category</h1>";
-
-
-
-    // } else 
-
-
-
-
-    while(mysqli_stmt_fetch($stmt)){
-       
-        
+    //   $stmt1 = mysqli_prepare($connection, "SELECT post_id, post_title, post_user, post_date, post_image, post_content FROM posts WHERE post_category_id = ?");
+    $adminPostsByCat = $postsByCat->adminPostsByCat();
+    foreach($adminPostsByCat as $row){
+        $post_id = $row['post_id'];
+        $post_title = $row['post_title'];
+        $post_author = $row['post_user'];
+        $post_author_id = $row['post_user_id'];
+        $post_date = $row['post_date'];
+        $post_image = $row['post_image'];
+        $post_content = $row['post_content'];
+        $post_status = $row['post_status'];
         ?>
-        
-
-
-                <!-- First Blog Post -->
-                <h2>
+        <h2>
                     <a href="/cms/post/<?php echo $post_id; ?>"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
@@ -109,16 +64,57 @@ if(isset($_SESSION['username']) && is_admin($_SESSION['username'])){
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-                
-   <?php }    }  else {
+                <?php 
+    }
 
 
 
 
-            header("Location: index.php");
+    } else {
+
+        //  $stmt2 = mysqli_prepare($connection, "SELECT post_id, post_title, post_user, post_date, post_image, post_content FROM posts WHERE post_category_id = ? AND post_status = ? ");
+
+        //  $published = 'published';
+        $PostsByCat = $postsByCat->PostsByCat();
+        foreach($PostsByCat as $row){
+            $post_id = $row['post_id'];
+            $post_title = $row['post_title'];
+            $post_author = $row['post_user'];
+            $post_author_id = $row['post_user_id'];
+            $post_date = $row['post_date'];
+            $post_image = $row['post_image'];
+            $post_content = $row['post_content'];
+            $post_status = $row['post_status'];
+            ?>
+            <h2>
+                        <a href="/cms/post/<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="author/<?php echo $post_author; ?>"><?php echo $post_author ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
+                    <hr>
+                    
+                    <img class="img-responsive" src="/cms/images/<?php if($post_image == ""){ echo "y9DpT.jpg"; } else{echo $post_image;}?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+    
+                    <hr>
+                    <?php 
+        }
+
+    }
+
+    } else {
+
+    redirect('/cms/');
+    
+    }
 
 
-   }?>
+
+?>
 
                 
                 
