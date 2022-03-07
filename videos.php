06c57d562dee726da787b27660620f2dc9a1ba8a
Ramonas-Nojus <ramonas.nojus@gmail.com>
2022-03-07 18:41:22 +0200

    add videos file and link on navigation

---
+++ videos.php
@@ -0,0 +1,47 @@
+<?php  include "includes/db.php"; ?>
+ <?php  include "includes/header.php"; ?>
+ <?php include "includes/class.autoload.php"; ?>
+
+
+    <!-- Navigation -->
+    
+    <?php  include "includes/navigation.php"; ?>
+
+    
+ 
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            
+            <div class="col-md-8">
+               
+             <?php
+
+      
+
+            ?>
+              
+
+            <!-- Blog Sidebar Widgets Column -->
+            
+            
+            <?php include "includes/sidebar.php";?>
+             
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+
+        <ul class="pager">
+
+
+        </ul>
+
+   
+
+<?php include "includes/footer.php";?>
