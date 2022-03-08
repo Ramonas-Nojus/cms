9fc20b3e3e62e4ca826a7df50271e4afa80de3b4
Ramonas-Nojus <ramonas.nojus@gmail.com>
2022-03-08 20:35:27 +0200

    add video liking

--- classes/Likes.class.php
+++ classes/Likes.class.php
@@ -3,14 +3,21 @@
 
 class Likes extends Db {
 
-    public function getLikes($post_id){
+    public function getLikesPost($post_id){
         $sql = "SELECT likes FROM posts WHERE post_id = ?";
         $stmt = $this->connection()->prepare($sql);
         $stmt->execute([$post_id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function setLikes($post_id,$user_id){
+    public function getLikesVideo($video_id){
+        $sql = "SELECT video_likes FROM videos WHERE video_id = ?";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute([$video_id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function setLikesPost($post_id,$user_id){
        
         $sql = "UPDATE posts SET likes = likes + 1 WHERE post_id = ?";
         $stmt = $this->connection()->prepare($sql);
@@ -22,7 +29,19 @@ class Likes extends Db {
         $stmt->execute([$user_id,$post_id]);
     }
 
-    public function unlike($post_id,$user_id){
+    public function setLikesVideo($video_id,$user_id){
+       
+        $sql = "UPDATE videos SET video_likes = video_likes + 1 WHERE video_id = ?";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute([$video_id]);
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $sql = "INSERT INTO likes(user_id,video_id) VALUES(?,?)";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute([$user_id,$video_id]);
+    }
+
+    public function unlikePost($post_id,$user_id){
        
         $sql = "UPDATE posts SET likes = likes - 1 WHERE post_id = ?";
         $stmt = $this->connection()->prepare($sql);
@@ -32,6 +51,18 @@ class Likes extends Db {
         $sql = "DELETE FROM likes WHERE post_id = ? AND user_id = ?";
         $stmt = $this->connection()->prepare($sql);
         $stmt->execute([$post_id, $user_id]);
+    }
+
+    public function unlikeVideo($video_id,$user_id){
+       
+        $sql = "UPDATE videos SET video_likes = video_likes - 1 WHERE video_id = ?";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute([$video_id]);
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $sql = "DELETE FROM likes WHERE video_id = ? AND user_id = ?";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute([$video_id, $user_id]);
     }
 
 }
